db62a2e6b90b77deec0478d569cc70e836d0ba94
Lawless1011 <54257675+Lawless1011@users.noreply.github.com>
2020-06-07 22:08:06 +0900

    Update Directory.php

--- src/net/mydeacy/serverdocument/elements/interfaces/Directory.php
+++ src/net/mydeacy/serverdocument/elements/interfaces/Directory.php
@@ -4,5 +4,5 @@ namespace net\mydeacy\serverdocument\elements\interfaces;
 
 interface Directory extends Element {
 
-	const BUTTON_IMAGE = "textures/blocks/bookshelf.png";
+	const BUTTON_IMAGE = "textures/blocks/barrel_side.png";
 }
